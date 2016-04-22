dc1375e972188356b70fae13ec072e4960d07a64
Chris Hamons <chris.hamons@xamarin.com>
2016-04-22 15:22:46 -0500

    Initial Commit

---
+++ MacPhotoDemo/MacPhotoDemo/ViewController.designer.cs
@@ -0,0 +1,42 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace MacPhotoDemo
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		AppKit.NSTextView DetailView { get; set; }
+
+		[Outlet]
+		AppKit.NSButton PickPhotoButton { get; set; }
+
+		[Outlet]
+		AppKit.NSImageView ThePhoto { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (PickPhotoButton != null) {
+				PickPhotoButton.Dispose ();
+				PickPhotoButton = null;
+			}
+
+			if (ThePhoto != null) {
+				ThePhoto.Dispose ();
+				ThePhoto = null;
+			}
+
+			if (DetailView != null) {
+				DetailView.Dispose ();
+				DetailView = null;
+			}
+		}
+	}
+}
